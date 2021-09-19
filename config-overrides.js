5781cb23eec63deeae83e27729c8b098ccc0c64c
vvt717@mail.ru <vvt717@mail.ru>
2021-09-20 00:03:43 +0300

    fix

---
+++ config-overrides.js
@@ -0,0 +1,12 @@
+const {
+    override,
+    addWebpackAlias
+} = require('customize-cra');
+
+const path = require("path");
+
+module.exports = override(
+    addWebpackAlias({
+        "@src": path.resolve(__dirname, 'src'),
+    }),
+);
